1b59decad9044c21d2fd4eac3224d285a1c24938
Nova <69091361+qzxtu@users.noreply.github.com>
2022-08-27 15:23:10 -0500

    Update GoogleTranslatorAutoDetect.cs

--- GoogleTranslatorAutoDetect.cs
+++ GoogleTranslatorAutoDetect.cs
@@ -1,10 +1,12 @@
 public static HttpClient hclient = new();
 
-public static string LanguageDetector(string input, string inputlanguage = "auto", string outputlanguage = "en")
+public static string LanguageDetector(string input)
 {
-  string url = string.Format("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}", inputlanguage, outputlanguage, Uri.EscapeUriString(input));
-  string args = hclient.GetStringAsync(url).Result;
+  var inputlanguage = "auto";
+  var outputlanguage = "en";
+  var url = string.Format("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}", inputlanguage, outputlanguage, Uri.EscapeUriString(input));
+  var args = hclient.GetStringAsync(url).Result;
   var detected = Regex.Match(args, @"],null,(.+?),null,null").Groups[1].Value;
-  string result = detected.Replace('"', ' ');
+  var result = detected.Replace('"', ' ');
   return result;
  }
