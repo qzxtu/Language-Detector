ea76831de1301085e13b5b3a9cb873a5e8ee286f
Nova <69091361+qzxtu@users.noreply.github.com>
2022-08-27 15:20:38 -0500

    Add files via upload

---
+++ GoogleTranslatorAutoDetect.cs
@@ -0,0 +1,8 @@
+public static string LanguageDetector(string input, string inputlanguage = "auto", string outputlanguage = "en")
+{
+  string url = string.Format("https://translate.googleapis.com/translate_a/single?client=gtx&sl={0}&tl={1}&dt=t&q={2}", inputlanguage, outputlanguage, Uri.EscapeUriString(input));
+  string args = Globals.hclient.GetStringAsync(url).Result;
+  var detected = Regex.Match(args, @"],null,(.+?),null,null").Groups[1].Value;
+  string result = detected.Replace('"', ' ');
+  return result;
+ }
